24cc8fe221b7e96e477d42098ccc6c67c75efeb7
albertoloriggio <alberto.loriggio@alumni.esade.edu>
2024-10-16 15:16:33 +0200

    commit

---
+++ assignment_1/ex.2.py
@@ -0,0 +1,6 @@
+a=3
+b=7
+print('the sum is',a+b)
+print('the difference is',a-b)
+print('the product is',a*b)
+print('the quotient is',a/b)
---
+++ assignment_1/ex.4.py
@@ -0,0 +1,4 @@
+universities=['uib','uab','ie','uoc','esade']
+print(universities)
+print(universities[0])
+print(universities[-1])
