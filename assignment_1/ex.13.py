2b417a9208edd6b736826149d2ce3b515e2ac962
albertoloriggio <alberto.loriggio@alumni.esade.edu>
2024-10-21 16:27:12 +0200

    commit

--- assignment_1/ex.13.py
+++
@@ -1,9 +0,0 @@
-def square(number):
-    float(number)
-    return(number**2)
-num1=square(5)
-num2=square(-3)
-num3=square(1.5)
-print(num1)
-print(num2)
-print(num3)
--- assignment_1/ex.16.py
+++
@@ -1,6 +0,0 @@
-students_grades={'Alex':[6,7,4],'Anna':[9,7,9],'Antony':[8,7,6]}
-def average_grades(students):
-    for i in students:
-        average=(sum(students[i])/len(students[i]))
-        print(average)    
-average_grades(students_grades)   
